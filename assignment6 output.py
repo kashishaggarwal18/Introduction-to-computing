2ceb0109c9028b84c63e84b76648ce964ffe85ff
kashishaggarwal18 <104267424+kashishaggarwal18@users.noreply.github.com>
2022-07-03 20:49:05 +0530

    Add files via upload

---
+++ assignment6 output.py
@@ -0,0 +1,50 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: C:/Users/kashish Aggarwal/AppData/Local/Programs/Python/Python310/assignment 6.py
+Enter a number: 4
+4  is not a perfect number
+
+Enter a String: hello world
+hello world is not a palindrome
+
+Enter a number: 5
+      1 
+     1 1 
+    1 2 1 
+   1 3 3 1 
+  1 4 6 4 1 
+
+Enter a String: i ilke music
+No 'i ilke music' is not a panagram
+
+Enter a hyphen separated sentence: color-blue
+blue-color
+
+Student ID= 21102018
+Student ID= 21102018
+Student Name= Kashish Aggarwal
+Student ID= 21102018
+Student Name= Kashish Aggarwal
+Student Class= 1st Year
+
+True
+False
+True
+False
+
+Check whether the said classes are subclasses of the built-in object class or not.
+True
+True
+
+Enter number of elements : 6
+14
+-10
+-4
+440
+-399
+-42
+14 -10 -4
+None
+Enter the sequence of parantheses : (){}[]
+(){}[] - is balanced
